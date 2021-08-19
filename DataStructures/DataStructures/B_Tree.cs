4dcdb62ec6f93e3e5a7968be8481374e316ffa5e
andrespa612 <38359603+andrespa612@users.noreply.github.com>
2021-08-18 19:47:10 -0600

    Insert

--- DataStructures/DataStructures/B_Tree.cs
+++ DataStructures/DataStructures/B_Tree.cs
@@ -6,6 +6,47 @@ namespace DataStructures
 {
     class B_Tree<T> where T : IComparable
     {
+        public B_TreeNode<T> root;
+        public int degree;
+        public int minDegree;
 
+        B_Tree(int d)
+        {
+            root = null;
+            degree = d;
+            minDegree = d / 2;
+        }
+
+        void Insert(T k)
+        {
+            if(root == null)
+            {
+                root = new B_TreeNode<T>(minDegree, true);
+                root.keys[0] = k;
+                root.length = 1;
+            }
+            else
+            {
+                if(root.length == (degree - 1))
+                {
+                    B_TreeNode<T> newNode = new B_TreeNode<T>(degree, false);
+                    newNode.childs[0] = root;
+                    newNode.SplitChild(0, root);
+
+                    int i = 0;
+                    if(newNode.keys[0].CompareTo(k) == -1)
+                    {
+                        i++;
+                    }
+                    newNode.childs[i].InsertKey(k);
+
+                    root = newNode;
+                }
+                else
+                {
+                    root.InsertKey(k);
+                }
+            }
+        }
     }
 }
--- DataStructures/DataStructures/B_TreeNode.cs
+++ DataStructures/DataStructures/B_TreeNode.cs
@@ -6,5 +6,55 @@ namespace DataStructures
 {
     class B_TreeNode<T> where T : IComparable
     {
+        public T[] keys;
+        int maximun;
+        int minimum;
+        public B_TreeNode<T>[] childs;
+        public int length;
+        public bool leaf;
+
+        public B_TreeNode(int degree, bool l)
+        {
+            maximun = degree - 1;
+            minimum = degree / 2 - 1;
+            leaf = l;
+            keys = new T[maximun];
+            childs = new B_TreeNode<T>[maximun + 1];
+            length = 0;
+        }
+
+        public void InsertKey(T k)
+        {
+            int i = length - 1;
+
+            if( leaf )
+            {
+                while (i >= 0 && keys[i].CompareTo(k) == 1)
+                {
+                    keys[i + 1] = keys[i];
+                    i--;
+                }
+
+                keys[i + 1] = k;
+                length++;
+            }
+            else
+            {
+                while (i >= 0 && keys[i].CompareTo(k) == 1)
+                {
+                    i--;
+                }
+                if(childs[i + 1].length == maximun)
+                {
+                    SplitChild(i + 1, childs[i + 1]);
+
+                    if(keys[i + 1].CompareTo(k) == -1)
+                    {
+                        i++;
+                    }
+                }
+                childs[i + 1].InsertKey(k);
+            }
+        }
     }
 }
